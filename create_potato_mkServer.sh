990bb2f11bd1ff9031cdf92fe5f97e86d6ed6112
ClaudiusMinimus <claudiusminimus@gmail.com>
2021-02-07 22:21:40 -0700

    copied inital configs to server

---
+++ create_potato_mkServer.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#change to acutal build directory if running from main dir
+cd /home/helaman/.local/share/multimc/instances/Create_Potato_build
+
+/home/helaman/Downloads/Minecraft/modpacks/mkServer/mkServer.sh 'create-potato-1.16.5-1.4.0' ./server
