c513cd0e24eb6a4916f73138d08b01982290262b
ClaudiusMinimus <claudiusminimus@gmail.com>
2021-06-11 11:21:58 -0600

    add event for discord link in chat

--- minecraft/kubejs/server_scripts/script.js
+++ minecraft/kubejs/server_scripts/script.js
@@ -18,3 +18,13 @@ onEvent('item.tags', event => {
   // Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
   // event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
 })
+
+events.listen('player.chat', function (event) {
+  // Check if message equals discord, ignoring case
+  if (event.message.trim().equalsIgnoreCase('discord')) {
+    // Schedule task in 1 tick
+    event.server.scheduleInTicks(1, event.server, function (callback) {
+      callback.data.tell(Text.of([Text.green('Join the official discord '), Text.blue('here').click('https://discord.com/invite/jXrx4Tu')]));
+    })
+  }
+})
